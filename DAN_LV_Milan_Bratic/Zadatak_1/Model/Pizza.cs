06bce5f5b8af6eb6d1d1dac3e0cebc4d454cb618
MilanBratic0512 <milan.bratic2@gmail.com>
2020-08-24 11:40:53 +0200

    create model, relay command and viewmodelbase

---
+++ DAN_LV_Milan_Bratic/Zadatak_1/Model/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Model
+{
+    class Pizza
+    {
+        public List<PizzaIngredient> PizzaIngredients { get; set; }
+        public int Size { get; set; }
+        public int TotalPrice { get; set; }
+
+
+    }
+}
---
+++ DAN_LV_Milan_Bratic/Zadatak_1/Model/PizzaIngredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Model
+{
+    class PizzaIngredient
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+    }
+}
